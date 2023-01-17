fb94c88d7df1a4531e4e8e7e5aabd73b8be4e188
Devkishen Sisodia <dsisodia@uoregon.edu>
2023-01-16 18:53:03 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Create a welcome message.
+# Input: a name as a string.
+# Result: a string.
+def welcome_message(name: str) -> str:
+    message = "Hello, " + name + "!"
+    return message
+
+
+message = welcome_message("dsisodia@calpoly.edu")
+print(message)
