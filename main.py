d719a374e70db86051d8c83f99a4a77cda2d3247
Madison Lopez <madison.lopez.8404@gmail.com>
2023-01-18 15:23:59 -0800

    Added my email to the welcome message.

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ def welcome_message(name: str) -> str:
     return message
 
 
-message = welcome_message("madison.lopez.8404@gmail.com")
+message = welcome_message("madison.lopez.8404@gmail.com"
 print(message)
